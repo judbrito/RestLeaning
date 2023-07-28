a7a6eed5d7f06eb6e07220b36e46d441e1b7f081
judbrito <admoceano@gmail.com>
2023-07-28 15:38:44 -0300

    aula 08

--- src/main/java/rest/OlaMundoTest.java
+++ src/main/java/rest/OlaMundoTest.java
@@ -1,5 +1,12 @@
 package rest;
 
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.hasSize;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.hamcrest.Matchers;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -22,5 +29,35 @@ public class OlaMundoTest {
 
 		ValidatableResponse validacao = response.then();
 		validacao.statusCode(200);
+
+	}
+
+	@Test
+	public void devoConhecerOutrasFormasRestAssured() {
+		Response response = RestAssured.request(Method.GET, "http://restapi.wcaquino.me:80/ola");
+		Assert.assertTrue(response.getBody().asString().equals("Ola Mundo!"));
+		ValidatableResponse validacao = response.then();
+		validacao.statusCode(200);
+		given().when().get("http://restapi.wcaquino.me/ola").then().statusCode(200);
+	}
+
+	@Test
+	public void exemploTresDevoConhecer() {
+		RestAssured.get("http://restapi.wcaquino.me/ola").then().statusCode(200);
+		System.out.println("uma linha codigo para verificar o status code");
+
+	}
+
+	@Test
+	public void devoConhecerMatchersHamcrest() {
+		Assert.assertThat("Maria", Matchers.is("MAria"));
+		Assert.assertThat(128, Matchers.is(128));
+		Assert.assertThat(128, Matchers.isA(Integer.class));
+		Assert.assertThat(128, Matchers.isA(Double.class));
+		Assert.assertThat(128d, Matchers.greaterThan(128d));
+		Assert.assertThat(128d, Matchers.lessThan(120d));
+
+		List<Integer> impares = Arrays.asList(1, 3, 5, 7, 9);
+		Assert.assertThat(impares, hasSize(5));
 	}
 }
