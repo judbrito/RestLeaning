327c3816d38608f1a1b653812a627deb30c551a2
judbrito <admoceano@gmail.com>
2023-07-31 15:15:57 -0300

    aula 13

--- src/main/java/rest/UserJasonTest.java
+++ src/main/java/rest/UserJasonTest.java
@@ -3,6 +3,9 @@ package rest;
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.containsString;
 import static org.hamcrest.Matchers.greaterThan;
+import static org.hamcrest.Matchers.hasItem;
+import static org.hamcrest.Matchers.hasItems;
+import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.Matchers.is;
 
 import org.junit.Assert;
@@ -49,6 +52,17 @@ public class UserJasonTest {
 	public void deveVerificarSegundoNivel() {
 		given().when().get("https://restapi.wcaquino.me/users/2").then().statusCode(200)
 				.body("name", containsString("Joaquina")).body("endereco.rua", is("Rua dos bobos"));
+	}
+
+	@Test
+	public void deveVerificarLista() {
+	    given().when().get("https://restapi.wcaquino.me/users/3").then().statusCode(200)
+	            .body("name", containsString("Ana"))
+	            .body("filhos", hasSize(2))
+	            .body("filhos[0].name", is("Zezinho"))
+	            .body("filhos[1].name", is("Luizinho"))
+	            .body("filhos.name", hasItem("Zezinho"))
+	            .body("filhos.name", hasItems("Zezinho", "Luizinho"));
 	}
 
 }
