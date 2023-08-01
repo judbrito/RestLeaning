835b35fd251689297c5e74747c64edf41e197943
judbrito <admoceano@gmail.com>
2023-08-01 15:19:50 -0300

    aula 16

--- src/main/java/rest/UserJasonTest.java
+++ src/main/java/rest/UserJasonTest.java
@@ -1,6 +1,11 @@
 package rest;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.anyOf;
+import static org.hamcrest.Matchers.arrayContaining;
+import static org.hamcrest.Matchers.arrayWithSize;
+import static org.hamcrest.Matchers.closeTo;
 import static org.hamcrest.Matchers.contains;
 import static org.hamcrest.Matchers.containsString;
 import static org.hamcrest.Matchers.greaterThan;
@@ -8,11 +13,11 @@ import static org.hamcrest.Matchers.hasItem;
 import static org.hamcrest.Matchers.hasItems;
 import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.lessThan;
 
 import java.util.Arrays;
 
 import org.junit.Assert;
-import org.junit.BeforeClass;
 import org.junit.Test;
 
 import io.restassured.RestAssured;
@@ -21,12 +26,12 @@ import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 
 public class UserJasonTest {
-
-	@BeforeClass
-	public static void setup() {
-		// Desabilitar a verificação do certificado SSL
-		RestAssured.useRelaxedHTTPSValidation();
-	}
+//
+//	@BeforeClass
+//	public static void setup() {
+//		// Desabilitar a verificação do certificado SSL
+//		RestAssured.useRelaxedHTTPSValidation();
+//	}
 
 	@Test
 	public void deveVerificarPrimeiroNivel() {
@@ -75,6 +80,28 @@ public class UserJasonTest {
 		given().when().get("https://restapi.wcaquino.me/users").then().statusCode(200).body("$", hasSize(3))
 				.body("name", hasItems("João da Silva", "Maria Joaquina", "Ana Júlia")).body("age[1]", is(25))
 				.body("filhos.name", hasItem(Arrays.asList("Zezinho", "Luizinho")))
-				.body("salary", contains(1234.578f, 2500, null));
+				.body("salary", contains(1234.5677f, 2500, null));
 	}
+
+	@Test
+	public void deveVerificacoesAvancadas() {
+		given().when().get("https://restapi.wcaquino.me/users").then().statusCode(200).body("$", hasSize(3))
+				.body("age.findAll{it<=25}.size()", is(2)).body("age.findAll{it<=25&&it>20}.size()", is(1))
+				.body("findAll{it.age <=25 && it.age>20}.name", hasItem("Maria Joaquina"))
+				.body("findAll{it.age<=25}[0].name", is("Maria Joaquina"))
+				.body("findAll{it.age<=25}[-1].name", is("Ana Júlia"))
+				.body("find{it.age<=25}.name", is("Maria Joaquina"))
+				.body("findAll{it.name.contains('n')}.name", hasItems("Maria Joaquina", "Ana Júlia"))
+				.body("findAll{it.name.length()>10}.name", hasItems("João da Silva", "Maria Joaquina"))
+				.body("name.collect{it.toUpperCase()}", hasItem("MARIA JOAQUINA"))
+				.body("name.findAll{it.startsWith('Maria')}.collect{it.toUpperCase()}", hasItem("MARIA JOAQUINA"))
+				.body("name.findAll{it.startsWith('Maria')}.collect{it.toUpperCase()}.toArray()",
+						anyOf(arrayContaining("MARIA jOAQUINA"), arrayWithSize(1)))
+				.body("age.collect{it*2}", hasItems(60, 50, 40)).body("id.max()", is(3))
+				.body("salary.min()", is(1234.5678f))
+				.body("salary.findAll{it !=null}.sum()", is(closeTo(3734.5678f, 0.001)))
+				.body("salary.findAll{it !=null}.sum()", allOf(greaterThan(3000d), lessThan(5000d)));
+
+	}
+
 }
