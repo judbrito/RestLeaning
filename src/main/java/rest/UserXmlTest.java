98205b8b85bf092b233f1158ef93b9d5aebff4d8
judbrito <admoceano@gmail.com>
2023-08-07 10:42:09 -0300

    Concluído aula 21

--- src/main/java/rest/UserXmlTest.java
+++ src/main/java/rest/UserXmlTest.java
@@ -5,22 +5,26 @@ import static org.hamcrest.Matchers.hasItem;
 import static org.hamcrest.Matchers.hasItems;
 import static org.hamcrest.Matchers.is;
 
+import java.util.ArrayList;
+
+import org.junit.Assert;
 import org.junit.Test;
 
+import io.restassured.internal.path.xml.NodeImpl;
+
 public class UserXmlTest {
-	@Test
+	// @Test
 	public void devoTrabalharComXml() {
-		given().when().get("https://restapi.wcaquino.me/usersXMl/3").then().statusCode(200)
+		given().when().get("https://restapi.wcaquino.me/usersXML/3").then().statusCode(200)
 				.body("user.name", is("Ana Julia")).body("user.@id", is("3")).body("user.filhos.name.size()", is(2))
 				.body("user.filhos.name[0]", is("Zezinho")).body("user.filhos.name[1]", is("Luizinho"))
-				.body("user.filhos.name", hasItem("Luisinho")).body("user.filhos.name", hasItems("Zezinho", "Luisinho"))
+				.body("user.filhos.name", hasItem("Luizinho")).body("user.filhos.name", hasItems("Zezinho", "Luizinho"))
 				.body("user.filhos.name.size()", is(2));
-
 	}
 
-	@Test
+	// @Test
 	public void devoTrabalharComXmlExemplo2() {
-		given().when().get("https://restapi.wcaquino.me/usersXMl/3").then().statusCode(200)
+		given().when().get("https://restapi.wcaquino.me/usersXML/3").then().statusCode(200)
 
 				.rootPath("user").body(".name", is("Ana Julia")).body("@id", is("3"))
 
@@ -28,20 +32,29 @@ public class UserXmlTest {
 
 				.detachRootPath("filhos").body("filhos.name[0]", is("Zezinho")).body("filhos.name[1]", is("Luizinho"))
 
-				.appendRootPath("filhos").body("name", hasItem("Luisinho")).body("name.size()", is(2))
-				.body("name", hasItems("Zezinho", "Luisinho"));
-
+				.appendRootPath("filhos").body("name", hasItem("Luizinho")).body("name.size()", is(2))
+				.body("name", hasItems("Zezinho", "Luizinho"));
 	}
 
-	@Test
+	// @Test
 	public void devoFazerPesquisasAvancadas() {
-		given().when().get("https://restapi.wcaquino.me/usersXMl").then().statusCode(200)
-				.body("users.user.size()", is(3)).body("users.user.findAll{it.age.toInterger()<=25}.size()", is(2))
+		given().when().get("https://restapi.wcaquino.me/usersXML").then().statusCode(200)
+				.body("users.user.size()", is(3)).body("users.user.findAll{it.age.toInteger()<=25}.size()", is(2))
 				.body("users.user.@id", hasItems("1", "2", "3"))
 				.body("users.user.findAll{it.name.toString().contains('n')}.name",
 						hasItems("Maria Joaquina", "Ana Julia"))
-				.body("users.user.salary,find{it.!null}.toDouble()", is(1234.5678d))
-				.body("users.user.age.collect{it.toInterger()*2}", hasItems(40, 50, 60));
+				.body("users.user.salary.find{it!=null}.toDouble()", is(1234.5678d))
+				.body("users.user.age.collect{it.toInteger()*2}", hasItems(40, 50, 60));
+	}
+
+	@Test
+	public void devoFazerPesquisasAvancadaComXmlEJava() {
+		ArrayList<NodeImpl> nomes = given().when().get("https://restapi.wcaquino.me/usersXML").then().statusCode(200)
+				.extract().path("users.user.name.findAll{it.toString().contains('n')}");
+
+		Assert.assertEquals(2, nomes.size());
+		Assert.assertEquals("Maria Joaquina".toUpperCase(), nomes.get(0).toString().toUpperCase());
+		Assert.assertTrue("ANA JULIA".equalsIgnoreCase(nomes.get(1).toString()));
 
 	}
 }
