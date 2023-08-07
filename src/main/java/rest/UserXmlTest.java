c6be9e2a2573fb9806c5febf5c3dada9ec90f5b1
judbrito <admoceano@gmail.com>
2023-08-07 15:26:53 -0300

    Concluído aula 22

--- src/main/java/rest/UserXmlTest.java
+++ src/main/java/rest/UserXmlTest.java
@@ -1,8 +1,11 @@
 package rest;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.containsString;
 import static org.hamcrest.Matchers.hasItem;
 import static org.hamcrest.Matchers.hasItems;
+import static org.hamcrest.Matchers.hasXPath;
 import static org.hamcrest.Matchers.is;
 
 import java.util.ArrayList;
@@ -56,5 +59,22 @@ public class UserXmlTest {
 		Assert.assertEquals("Maria Joaquina".toUpperCase(), nomes.get(0).toString().toUpperCase());
 		Assert.assertTrue("ANA JULIA".equalsIgnoreCase(nomes.get(1).toString()));
 
+	}
+
+	@Test
+	public void devoFazerPesquisasAvancadaComXpath() {
+		given().when().get("https://restapi.wcaquino.me/usersXML").then().statusCode(200)
+				.body(hasXPath("count(/users/user)", is("3"))).body(hasXPath("/users/user[@id='1']"))
+				.body(hasXPath("//user[@id='2']"))
+				.body(hasXPath("//name[text()='Luizinho']/../../name", is("Ana Julia")))
+				.body(hasXPath("//name[text()='Ana Julia']/following-sibling::filhos",
+						allOf(containsString("Zezinho"), containsString("Luizinho"))))
+				.body(hasXPath("/users/user/name", is("João da Silva"))).body(hasXPath("//name", is("João da Silva")))
+				.body(hasXPath("/users/user[2]/name", is("Maria Joaquina")))
+				.body(hasXPath("/users/user[lastName()]/name", is("Ana Julia	")))
+				.body(hasXPath("count(/users/user[contains(.,'n')])", is("2")))
+				.body(hasXPath("//user[age<24]/name", is("Ana Julia	")))
+				.body(hasXPath("//user[age>24 and age <30]/name", is("Maria Joaquina")))
+				.body(hasXPath("//user[age>24][age <30]/name", is("Maria Joaquina")));
 	}
 }
