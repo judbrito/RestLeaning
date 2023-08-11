cd2f400f87cb1a8e6f0198ea1f857b106559e523
judbrito <admoceano@gmail.com>
2023-08-11 16:08:44 -0300

    aula 27

--- src/main/java/rest/VerbosTest.java
+++ src/main/java/rest/VerbosTest.java
@@ -6,12 +6,21 @@ import static org.hamcrest.Matchers.notNullValue;
 
 import org.junit.Test;
 
+import io.restassured.http.ContentType;
+
 public class VerbosTest {
-	@Test
-	public void devoSalvarUsuário() {
+	// @Test
+	public void devoSalvarUsuario() {
 		given().log().all().contentType("application/json").body("{\"name\":\"jose\",\"age\":50}").when()
 				.post("https://restapi.wcaquino.me/users").then().log().all().statusCode(201)
 				.body("id", is(notNullValue())).body("name", is("jose")).body("age", is(50));
 
+	}
+
+	@Test
+	public void devoSalvarUsuarioViaXml() {
+		given().contentType(ContentType.XML).body("<user><name>Jose</name><age>50</age></user>").log().all().when()
+				.post("https://restapi.wcaquino.me/usersXMl").then().log().all().statusCode(201)
+				.body("user.@id", is(notNullValue())).body("user.name", is("Jose")).body("user.age", is("50"));
 	}
 }
