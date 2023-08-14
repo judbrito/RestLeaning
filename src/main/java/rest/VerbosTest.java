aa10283e6f5bb9e0039ea80c17aadde79afac31e
judbrito <admoceano@gmail.com>
2023-08-14 11:03:24 -0300

    aula 31

--- src/main/java/rest/VerbosTest.java
+++ src/main/java/rest/VerbosTest.java
@@ -4,6 +4,10 @@ import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.notNullValue;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.commons.lang3.CharEncoding;
 import org.junit.Test;
 
 import io.restassured.http.ContentType;
@@ -18,10 +22,22 @@ public class VerbosTest {
 	}
 
 	@Test
+	public void devoSalvarUsuarioUsandoMap() {
+		Map<String, Object> params = new HashMap<String, Object>();
+		params.put("name", "Usuário via Map");
+		params.put("age", 25);
+		given().body(params).log().all().contentType("application/json").body("{\"name\":\"jose\",\"age\":50}").when()
+				.post("https://restapi.wcaquino.me/users").then().log().all().statusCode(201)
+				.body("id", is(notNullValue())).body("name", is("jose")).body("age", is(50));
+
+	}
+
+	@Test
 	public void devoSalvarUsuarioViaXml() {
-		given().contentType(ContentType.XML).body("<user><name>Jose</name><age>50</age></user>").log().all().when()
+		given().contentType(ContentType.XML.withCharset(CharEncoding.UTF_8))
+				.body("<user><name>José</name><age>50</age></user>").log().all().when()
 				.post("https://restapi.wcaquino.me/usersXMl").then().log().all().statusCode(201)
-				.body("user.@id", is(notNullValue())).body("user.name", is("Jose")).body("user.age", is("50"));
+				.body("user.@id", is(notNullValue())).body("user.name", is("José")).body("user.age", is("50"));
 	}
 
 	@Test
