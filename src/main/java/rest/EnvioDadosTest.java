6a9ef4b4aa37bb9ce9e20185d96d1b37ddb549e0
judbrito <admoceano@gmail.com>
2023-08-15 11:48:07 -0300

    aula 38

--- src/main/java/rest/EnvioDadosTest.java
+++ src/main/java/rest/EnvioDadosTest.java
@@ -20,4 +20,10 @@ public class EnvioDadosTest {
 				.get("https://restapi.wcaquino.me/v2/users").then().log().all().statusCode(200)
 				.contentType(ContentType.XML).contentType(containsString("utf-8"));
 	}
+
+	@Test
+	public void deveEnviarValorViaHeader() {
+		given().log().all().accept(ContentType.JSON).when().get("https://restapi.wcaquino.me/v2/users").then().log()
+				.all().statusCode(200).contentType(ContentType.JSON);
+	}
 }
