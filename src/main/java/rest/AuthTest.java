df94aa11d7afde3ab564c3ef774cc72111c52f91
judbrito <admoceano@gmail.com>
2023-08-17 14:46:09 -0300

    aula 48

--- src/main/java/rest/AuthTest.java
+++ src/main/java/rest/AuthTest.java
@@ -1,6 +1,7 @@
 package rest;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.greaterThan;
 import static org.hamcrest.Matchers.is;
 
 import org.junit.Test;
@@ -19,4 +20,21 @@ given()
 .body("name", is("Luke Skywalker"))
 ;
 }
+@Test
+public void deveObterclima() {
+given()
+.log().all()
+.queryParam("q","Fortaleza,BR")
+.queryParam("appid","6bfe1ceed23fa95ff2c38a8353e830de")
+.queryParam("units","metric")
+.when()
+.get("https://api.openweathermap.org/data/2.5/weather")
+.then()
+.log().all()
+.statusCode(200)
+.body("name", is("Fortaleza"))
+.body("coord.lon", is(-38.5247f))
+.body("main.temp", greaterThan(25f));
 }
+}
+//https://api.openweathermap.org/data/2.5/weather?q=Fortaleza,BR&appid=6bfe1ceed23fa95ff2c38a8353e830de&units=metric
