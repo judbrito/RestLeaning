46905e1f8430d84507ebfa0c55f9e7ab223d5f6c
judbrito <admoceano@gmail.com>
2023-08-18 12:16:50 -0300

    aula 51

--- src/main/java/rest/AuthTest.java
+++ src/main/java/rest/AuthTest.java
@@ -11,6 +11,8 @@ import java.util.Map;
 import org.junit.Test;
 
 import io.restassured.http.ContentType;
+import io.restassured.path.xml.XmlPath;
+import io.restassured.path.xml.XmlPath.CompatibilityMode;
 
 public class AuthTest {
 	// endereço endpoint atualizada!
@@ -60,7 +62,8 @@ public class AuthTest {
 		login.put("senha","123456");
 		//login na api
 		//receber token
-	String token	= given().log().all()
+	String token	= given()
+			.log().all()		
 			.body(login)
 			.contentType(ContentType.JSON)
 			.when()
@@ -74,6 +77,40 @@ public class AuthTest {
 			.get("http://barrigarest.wcaquino.me/contas")
 			.then().log().all().statusCode(200)
 		.body("nome", hasItem ("judbrito"));
+	
+	}
+	@Test
+	public void deveAcessarAplicacaoWeb() {
+		//login
+		String cookie	= given()
+				.log().all()
+				.formParam("email","brito@brito")
+				.formParam("senha","123456")
+				.contentType(ContentType.URLENC.withCharset("UTF-8"))
+				.when()
+				.post("http://seubarriga.wcaquino.me/logar")
+				.then()
+				.log().all()
+				.statusCode(200)
+				.extract().header("set-cookie");
+		
+		cookie = cookie.split("=")[1].split(";")[0];
+		System.out.println(cookie);
+		//obter conta
+		String body = given()
+		.log().all()
+		.cookie("connect.sid", cookie)
+		.when()
+			.get("http://seubarriga.wcaquino.me/contas")
+			.then()
+			.log().all()
+			.statusCode(200)
+		.body("html.body.table.tbody.tr[0].td[0]", is ("Conta Teste"))
+		.extract().body().asString();
+		
+		System.out.println("______________");
+		XmlPath xmlpath = new XmlPath(CompatibilityMode.HTML, body);
+		System.out.println(xmlpath.getString("html.body.table.tbody.tr[0].td[0]"));
 	
 	}
 }
